160b8f7460f73b7a585618fa7bc0c6cf8b9cdd06
TanyaArdhia <tanyaardhiap@gmail.com>
2024-08-02 20:44:17 +0700

    fix: app js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
+const cors = require("cors");
 const router = require("./routers");
 const app = express();
 const port = 3000;
@@ -10,6 +11,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use(router);
 
 app.get("/", (req, res) => {
